787d43b91b1f268954f190ff5a0bd57abbc0bb35
Михаил <misha980405@gmail.com>
2020-11-23 18:54:06 +0200

    Выполнено постороение шаблона по образцу

---
+++ home 4/header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css" >
+</head>
+<body>
+<div class="section first-section">
+	<span class="container">
+		<h1>Long headline for two lines for attention and evolving</h1>
+		<h2>
+			With the new AirPlay 2, you can control your home audio system
+			and the speakers throughout your house.4 You can play a song
+			in the living room and your kitchen at the same time.
+		</h2>
+	</span>
+	<span class="vector"><img src="logo/Vector.svg"></span>
+	<span class="vector1"><img src="logo/Vector%20(1).svg"></span>
+	<span class="logo"><img src="logo/Logo.svg"></span>
+	<span class="logo1"><img src="logo/Logo%20(1).svg"></span>
+</div>
+<div class="section">
+	<div class="container">
+	<h3><span class="video"><img src="logo/Image.svg"></span>Medium length display headline </h3>
+	<p class="text">
+		Separated they live in Bookmarks right at the coast of the
+		famous Semantics, large language ocean Separated they live in
+		Bookmarks right
+
+
+
+	</p>
+		<p class="text">	<a href="https://www.google.ru">Action link</a></p>
+		<div class="section">
+			<h4>Medium Length Title</h4>
+			<h5>Medium length headline</h5>
+			<p class="text1">By creating an Account on our service, you agree to <a href="https://www.google.ru">subscribe to newsletters </a>,
+				marketing or promotional materials and other information we may send. However, you may opt
+				out of receiving any, or all, of these communications from us by following the unsubscribe
+				link or instructions provided in any email we send.</p>
+			<h5>Medium length headline</h5>
+			<p class="text1">However, you may opt out of receiving any, or all, of these communications from
+				us by following the unsubscribe link or instructions provided in any email we send.</p>
+			<p class="text1">By creating an Account on our service, you agree to subscribe to newsletters,
+				marketing or promotional materials and other information we may send. However, you may opt
+				out of receiving any, or all, of these communications from us by following the unsubscribe
+				link or instructions provided in any email we send. You understand that your content
+				(<a href="https://www.google.ru"> not including credit card information</a>), may be transferred unencrypted and involve (a)
+				transmissions over various networks; and (b) changes to conform and adapt to technical
+				requirements of connecting networks or devices. Credit card information is always encrypted
+				during transfer over networks.
+			</p>
+
+			<p class="text1 dot">
+				&middot; Showcase and embed your work with</p>
+			<p class="text1 dot1">
+				&middot; Publish across social channels in a click</p>
+			<p class="text1 dot1">
+				&middot; Sell your videos worldwide</p>
+			<p class="text1 dot1">
+				&middot; Embed your work with</p>
+		</div>
+</div>
+</body>
+</html>
