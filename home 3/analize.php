d182714bdb3e897742e08ab6b06021226906043e
Михаил <misha980405@gmail.com>
2020-12-08 16:52:47 +0200

    Изменения вывода данных

--- home 3/analize.php
+++ home 3/analize.php
@@ -1,6 +1,6 @@
 <?php
-
-
+$res = analize('./');
+var_export($res);
 function analize($input)
 {
 	$list =(['folders'=>[],'files'=>[]]);
@@ -11,19 +11,19 @@ function analize($input)
 		}
 		if (is_dir($element)) {
 			$list['folders'][$element]['is_readable'] =
-				(is_readable($element) == true ? 'true' : 'false');
+				(is_readable($element) == true ? true : false);
 			$list['folders'][$element]['is_writable'] =
-				(is_writable($element) == true ? 'true' : 'false');
+				(is_writable($element) == true ? true : false);
 		}
 		if (is_file($element)) {
 			$list['files'][$element]['is_readable'] =
-				(is_readable($element) == true ? 'true' : 'false');
+				(is_readable($element) == true ? true : false);
 			$list['files'][$element]['is_writeable'] =
-				(is_writeable($element) == true ? 'true' : 'false');
+				(is_writeable($element) == true ? true : false);
 			$list['files'][$element]['filesize'] = filesize($element);
 		}
 	}
 	closedir($currentDir);
-	print_r($list);
+	return $list;
 
 }
--- home 3/main.php
+++ home 3/main.php
@@ -11,6 +11,7 @@ if ($input=='testing')
 else
 {
 	analize($input);
+
 }
 
 
